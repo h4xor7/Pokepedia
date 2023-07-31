35ac7161da5b11b6b98397cb7aad7ee3ae43ccdf
Knightmare <saurabhpandey436@gmail.com>
2023-07-31 21:54:17 +0530

    completed Retrofit Setup

---
+++ app/src/main/java/com/pandey/pokepedia/data/remote/responses/GenerationV.kt
@@ -0,0 +1,10 @@
+package com.pandey.pokepedia.data.remote.responses
+
+
+import com.google.gson.annotations.SerializedName
+import com.plcoding.jetpackcomposepokedex.data.remote.responses.BlackWhite
+
+data class GenerationV(
+    @SerializedName("black-white")
+    val blackWhite: BlackWhite
+)
---
+++ app/src/main/java/com/pandey/pokepedia/data/remote/responses/PokemonList.kt
@@ -0,0 +1,9 @@
+package com.pandey.pokepedia.data.remote.responses
+
+
+data class PokemonList(
+    val count: Int,
+    val next: String,
+    val previous: Any,
+    val results: List<Result>
+)
