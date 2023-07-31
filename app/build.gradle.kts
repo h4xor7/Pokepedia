0c19b34a69cf44435df8d55eb5270ba3323cad98
Knightmare <saurabhpandey436@gmail.com>
2023-07-31 22:17:45 +0530

    completed Retrofit Setup resolved viewmodel issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -108,7 +108,7 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.46.1")
     kapt("com.google.dagger:hilt-android-compiler:2.46.1")
 
-    implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+    //implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
     kapt ("androidx.hilt:hilt-compiler:1.0.0")
     implementation ("androidx.hilt:hilt-navigation-compose:1.1.0-alpha01")
 
